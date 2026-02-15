372fe7bdc9e7099d6cff911364c45bc08ef373fd
Mike <172455720+polarbots@users.noreply.github.com>
2026-02-15 15:10:59 -0500

    Added button to set number of squares per side

--- etchasketchScript.js
+++ etchasketchScript.js
@@ -2,6 +2,26 @@
 // Select container div to assign other divs to later
 const container = document.querySelector('#container');
 
+// Set default value to numSquarePerSide
+let numSquarePerSide = 16
+
+createGrid(numSquarePerSide);
+
+// Set event listener on button click to prompt user to enter number for numSquarePerSide
+const numSquareButton = document.querySelector('button');
+numSquareButton.addEventListener("click", () => {
+    // Cast to number, then round to get an integer
+    let userInput = Math.round(Number(prompt(`How many squares do you want per side?\nThis value can be any integer from 1 to 99 and defaults to ${numSquarePerSide}.`)));
+    console.log(userInput);
+
+    // Verify input is an int from 1-99. If so, assign to numSquarePerSide
+    if (userInput >= 1 && userInput <= 99) {
+        numSquarePerSide = userInput;
+    }
+
+    createGrid(numSquarePerSide);
+});
+
 // Add a div of class square as child of a div with class row
 function addSquareDivToRow(row) {
     const squareDiv = document.createElement('div');
@@ -22,12 +42,14 @@ function addSquareDivToRow(row) {
     row.appendChild(squareDiv);
 }
 
-// Create 16 row divs each containing 16 square divs for a 16x16 grid and assign as child of container
-for (let i = 0; i < 16; i++) {
+// Create a grid of size _x_ as determined by user input
+function createGrid(numSquarePerSide) {
+    for (let i = 0; i < numSquarePerSide; i++) {
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row');
-    for (j = 0; j < 16; j++) {
+    for (j = 0; j < numSquarePerSide; j++) {
         addSquareDivToRow(rowDiv);
     };
     container.appendChild(rowDiv);
+    };
 }
